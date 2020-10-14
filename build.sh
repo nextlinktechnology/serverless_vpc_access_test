4546927067f2b273bdea02f245bfbb4763ee9996
Sean Wilson <mrsean@gmail.com>
2020-10-14 14:52:40 +0800

    add axios calls remove mysql call

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 docker build -t cloud_run_connect_mysql .  
-docker tag ccai-avaya-demo gcr.io/serverless-vpc-access/cloud_run_connect_mysql
+docker tag cloud_run_connect_mysql gcr.io/serverless-vpc-access/cloud_run_connect_mysql
 docker push gcr.io/serverless-vpc-access/cloud_run_connect_mysql
--- index.js
+++ index.js
@@ -1,13 +1,74 @@
-var mysql = require("mysql");
+var axios = require("axios");
+var os = require("os");
 
-var con = mysql.createConnection({
-  host: "10.146.0.2",
-  port: "3306",
-  user: "root",
-  password: "root",
+// var mysql = require("mysql");
+// mysql -u root -pTaiwan2020! -h 10.146.0.2 -P3306
+// var con = mysql.createConnection({
+//   host: "10.146.0.2",
+//   port: "3306",
+//   user: "my-app-user",
+//   password: "Taiwan2020!",
+//   insecureAuth: true,
+// });
+
+// con.connect(function (err) {
+//   if (err) throw err;
+//   console.log("Connected!");
+// });
+
+var express = require("express");
+var bodyParser = require("body-parser");
+
+const expressApp = express().use(bodyParser.json());
+
+expressApp.get("/test", (request, response) => {
+  console.log("test");
+  response.send('{"a":"b", "c":"d"}');
 });
 
-con.connect(function (err) {
-  if (err) throw err;
-  console.log("Connected!");
+expressApp.get("/neg-test", (request, response) => {
+  console.log("neg-test");
+  response.send("neg-test");
 });
+
+expressApp.get("/", async (req, res) => {
+  try {
+    console.log(req.query);
+    console.log(`Get from: ${req.query.server}`);
+
+    var response = await axios({
+      method: "get",
+      url: req.query.server,
+    });
+    console.log("response");
+    // console.log(response);
+
+    res.send(response.data);
+  } catch (ex) {
+    console.log(`[ERROR]${ex}`);
+    res.send(ex);
+  }
+});
+
+expressApp.get("/getspecs", (req, res) => {
+  try {
+    let responseContent = {
+      hostname: os.hostname(),
+      type: os.type(),
+      platform: os.platform(),
+      arch: os.arch(),
+      uptime: os.uptime(),
+      nics: os.networkInterfaces(),
+    };
+    console.log("getspecs");
+    res.send(JSON.stringify(responseContent));
+    //res.send("xxx");
+  } catch (ex) {
+    console.log(`[ERROR]${ex}`);
+    res.send(ex);
+  }
+  //   res.send("xxx");
+});
+
+expressApp.listen(8080); //  Cloud Run now only supports port 8080
+console.log(`express server listening on port 8080...`);
